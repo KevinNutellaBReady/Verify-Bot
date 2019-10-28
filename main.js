5438b682207688a53ec784524513e350343655a1
KevinNutellaBReady <39007681+KevinNutellaBReady@users.noreply.github.com>
2019-10-29 00:17:10 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+
+const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+
+var client = new Discord.Client();
+
+client.on('ready', () => {
+
+    console.log(`Logged in as ${client.user.username}`);
+    client.user.setActivity('Use ' + config.prefix + config.command);
+
+});
+
+client.on('message', (msg) => {
+
+    const cont = msg.content,
+          author = msg.member
+
+    if(cont == config.prefix + config.command) {
+        var role = msg.guild.roles.find(role => role.name === config.rule);
+
+        author.addRole(role);
+        msg.reply(config.message);
+    }
+
+});
+
+
+client.login(config.token);
